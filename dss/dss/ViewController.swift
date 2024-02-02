f7cf9151a890e14e13207d58527e52e02650ee1b
Bagi <vitalibahdanau15@gmail.com>
2024-02-03 00:26:42 +0300

    part1 taks1

---
+++ dss/dss/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  dss
+//
+//  Created by Vitali Bahdanau on 3.02.24.
+//
+
+import UIKit
+
+// Task 2
+var integer: Int = 5
+var boolen: Bool = true
+var double: Double = 3.55
+var float: Float = 3.1
+var char: Character = "$"
+
+let inTeger: Int = 5
+let boOlen: Bool = true
+let doUble: Double = 3.55
+let flOat: Float = 3.1
+let stRing: String = "sddd"
+let chAr: Character = "$"
+
+Int8.min
+Int8.max
+
+
+Int16.min
+Int16.max
+
+Int32.min
+Int32.max
+
+Int.min
+Int.max
+
+UInt8.min
+UInt8.max
+
+UInt16.min
+UInt16.max
+
+UInt32.min
+UInt32.max
+
+UInt.min
+UInt.max
+
+
